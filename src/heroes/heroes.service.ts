c4a5241cfcc7178380db967f60a33bc90221bae1
Lucas Araujo <kirilucas@gmail.com>
2021-06-18 15:01:11 -0300

    Testing branch rules

--- src/heroes/heroes.service.ts
+++ src/heroes/heroes.service.ts
@@ -5,7 +5,7 @@ import { HeroesDto } from './dto/heroes.dto';
 export class HeroesService {
     
     // Used for testing purposes, should be deleted after
-    // the database starts being used
+    // the database starts being used 
     heroes: HeroesDto[] = [
         new HeroesDto("Thor Odinson", "Aesir", "Unknown", "Male", "Warrior", "Gods"),
         new HeroesDto("Odin Borson", "Aesir", "Unknown", "Male", "Warrior", "Gods"),
