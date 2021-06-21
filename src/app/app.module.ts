9d6ea32fd749b978f0156b46bf52303bbde8d2a9
Lucas Araujo <kirilucas@gmail.com>
2021-06-21 14:38:38 -0300

    Added .env / Changed powers name to skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { HeroesController } from "../heroes/heroes.controller"
-import { PowersController } from '../powers/powers.controller';
-import { HeroPowersController } from '../powers/heroes.controller';
+import { PowersController } from '../skills/powers.controller';
+import { HeroPowersController } from '../skills/heroes.controller';
 import { HeroesService } from 'src/heroes/heroes.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { HeroesModel } from 'src/heroes/heroes.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'admin',
-      database: 'rpg_tool',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
     }),
