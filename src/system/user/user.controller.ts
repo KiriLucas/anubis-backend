a680621770893be0d1af062b2ae35c50fa3fb3c0
Lucas Araujo <kirilucas@gmail.com>
2021-06-28 16:26:32 -0300

    Middlware testing

--- src/system/user/user.controller.ts
+++ src/system/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserCreationDto } from "./dtos/userCreation.dto";
 import { UserLoginDto } from "./dtos/userLogin.dto";
 import { UserResponseDto } from "./dtos/userResponse.dto";
 import { UserService } from "./user.service";
+import { Request } from 'express'
 
 @Controller('users')
 export class UserController {
@@ -20,5 +21,12 @@ export class UserController {
         const user = await this.userService.login(loginDto)
 
         return await this.userService.getUserResponse(user)
+    }
+
+    // Middlware testing
+    @Get('user')
+    async currentUser(@Req() request: Request): Promise<UserResponseDto> {
+        console.log(request)
+        return 'currentUser' as any
     }
 }
