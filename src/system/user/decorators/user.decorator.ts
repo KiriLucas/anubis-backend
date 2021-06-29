e7fbf8d910cd384dd8ae9b36a1f7f4b3a6f2735e
Lucas Araujo <kirilucas@gmail.com>
2021-06-29 09:38:46 -0300

    Authentication implementation

--- src/system/user/decorators/user.decorator.ts
+++ src/system/user/decorators/user.decorator.ts
@@ -1,15 +1,15 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
- export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
-     const request = ctx.switchToHttp().getRequest()
+export const User = createParamDecorator((data: any, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest()
 
-     if(!request.user){
-         null;
-     }
+    if (!request.user) {
+        null;
+    }
 
-     if(data){
-         return request.user[data]
-     }
+    if (data) {
+        return request.user[data]
+    }
 
-     return request.user
- })
+    return request.user
+})
