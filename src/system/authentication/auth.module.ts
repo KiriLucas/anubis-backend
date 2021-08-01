b586ab5b5f33cb7c259d769010abc294ba8ee21a
Lucas Araujo <kirilucas@gmail.com>
2021-07-31 21:07:20 -0300

    WIP

--- src/system/authentication/auth.module.ts
+++ src/system/authentication/auth.module.ts
@@ -1,9 +1,11 @@
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-    imports: [UserModule],
-    providers: [AuthService],
+    imports: [UserModule, PassportModule],
+    providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
