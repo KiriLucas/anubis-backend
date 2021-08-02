1ba10ef4bf013371a90481299348fe60740cd2de
Lucas Araujo <kirilucas@gmail.com>
2021-08-02 13:25:46 -0300

    WIP

--- src/system/authentication/auth.service.ts
+++ src/system/authentication/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { UserDto } from '../user/dtos/user.dto';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {}
-  
+    constructor(private userService: UserService, private jwtService: JwtService) { }
+
     async validateUser(username: string, password: string): Promise<UserDto> {
-      const user: UserDto = await this.userService.getUserByUsername(username); // TODO: Refactor UserDTO so it won't return the user's password
-      if (user && user.password === password) {
-        return user;
-      }
-      return null;
+        const user: UserDto = await this.userService.getUserByUsername(username); // TODO: Refactor UserDTO so it won't return the user's password
+        // TODO: Use bcrypt to check password
+        if (user && user.password === password) {
+            return user;
+        }
+        return null;
     }
-  }
+
+    async login(user: any) {
+        const payload = { username: user.username, sub: user.userId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
