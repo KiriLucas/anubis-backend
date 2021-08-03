facf8720831871936f6ea33bcd035e89216acb2a
Lucas Araujo <kirilucas@gmail.com>
2021-08-03 15:14:23 -0300

    Fixed some dependencies versions

--- src/system/authentication/auth.service.ts
+++ src/system/authentication/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
 
     async validateUser(username: string, password: string): Promise<UserDTO> {
         const user: UserDTO = await this.userService.getUserByUsername(username); // TODO: Refactor UserDTO so it won't return the user's password
-        console.log('batata: ', user)
+
         // TODO: Use bcrypt to check password
         if (user && user.password === password) {
             return user;
