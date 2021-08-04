dceff91b69a4963658627382cfef863ef0446f9e
Lucas Araujo <kirilucas@gmail.com>
2021-08-04 07:49:29 -0300

    WIP

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app/app.module';
-import { AuthGuard } from './system/authentication/guards/auth.guard';
 
 (async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  //app.useGlobalGuards(new AuthGuard)
   useContainer(app.select(AppModule), {fallbackOnErrors: true})
   await app.listen(3000);
 })();
--- src/system/authentication/auth.controller.ts
+++ src/system/authentication/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./guards/local-strategy.guard";
 
 @Controller('auth')
 export class AuthController {
 
+  constructor(private authService: AuthService) {}
+
     @UseGuards(LocalAuthGuard)
     @Post('/login')
     async login(@Request() request) {
-      return request.user;
+      return this.authService.login(request.user);
     }
 }
